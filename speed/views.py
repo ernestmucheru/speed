c800d2d2696f5e198ec467e2572e3346ad06f789
Ernest <ernestmucheru254@gmail.com>
2022-03-01 10:30:14 +0300

    intergrated mailgun into the contact functionality

--- speed/views.py
+++ speed/views.py
@@ -1,11 +1,13 @@
+from datetime import datetime
 from django.shortcuts import render
 import email
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.core.mail import send_mail,BadHeaderError
 from django.shortcuts import render,redirect
 # from .forms import ContactForm
 from django.core.mail import send_mail
 from django.contrib import messages
+from datetime import datetime, timezone
 
 # Create your views here.
 def home(request):
@@ -29,3 +31,17 @@ def home(request):
         send_mail(data['subject'],message, '', ['estmuch254@gmail.com'])
         messages.success(request, "Thank you for contacting us. We will get back to you shortly.")
     return render(request, 'base.html',{})
+
+# def process_request(request):
+#     #email function
+#     #subject
+#         subject = request.POST.get('subject')
+#         #message
+#         message = request.POST.get('message')
+#         #From email
+#         email = request.POST.get('email')
+
+        
+
+#     send_mail(data['subject'], message,'This is a test message body', ['estmuch254@gmail.com'])
+#     return HttpResponseRedirect("/")
