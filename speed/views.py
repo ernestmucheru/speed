c9f45a75062d79f9a1673732b30d3deffdc5ff70
Ernest <ernestmucheru254@gmail.com>
2022-03-01 11:27:29 +0300

    updated the send mail method

--- speed/views.py
+++ speed/views.py
@@ -12,13 +12,11 @@ from datetime import datetime, timezone
 # Create your views here.
 def home(request):
     if request.method == 'POST':
-        name = request.POST.get('full-name')
         email = request.POST.get('email')
         subject = request.POST.get('subject')
         message = request.POST.get('message')
 
         data = {
-            'name': name,
             'email': email,
             'subject': subject,
             'message': message
